c0ddf88d7f6e6f30dce41cbf384bd4d8afd5b9cc
Letícia Moreira <leticiamoreira789@gmail.com>
2024-05-10 17:03:21 -0300

    Testes script #35

--- codigo-fonte/retorno_finalizacao_pedidos/script.js
+++ codigo-fonte/retorno_finalizacao_pedidos/script.js
@@ -1,13 +1,15 @@
-const user = {
-    userName: "Leticia teste",
-    userEmail: "leticiamoreira789@gmail.com",
-    userTel: "35997442500",
-    userCategory: "tomador",
-    userPassword: "7788le",
-    notification: "true",
-}
+/* const produtoReservado = {
 
-const product = {
+  ,user {    
+  userName: "Leticia teste",
+  userEmail: "leticiamoreira789@gmail.com",
+  userTel: "35997442500",
+  userCategory: "tomador",
+  userPassword: "7788le",
+  notification: "true",
+},
+
+  product {
     productName: "cadeira de rodas 2000",
     productType: "cadeira de rodas",
     productCategory: "emprestimo",
@@ -15,9 +17,22 @@ const product = {
     productAdress: "37714-477",
     productDescription: "A cadeira de rodas 2000 é semi-nova, regulável e automática, é dobrável e está em perfeitas condições de uso.",
     productImg: "img class="img-fluid fotocadeirarodas" src="img/Screenshot_1.png"",
+
+  }, 
+}*/
+
+var pedido = new Object();
+var user = new Object();
+var product = new Object();
+
+locaStorage.setItem("userName","Leticia teste");
+
+function(){
+  var divdadosReserva = document.getElementById("dadosReserva");
+  divdadosReserva.innerHTML = userNameTeste;
 }
 
-const produtoReservado = window.localStorage.getItem('produto')
-if (produtoReservado) {
-  const produtoReservado = JSON.parse(produtoReservado)
+const userName = window.localStorage.getItem('userName')
+if (userName) {
+  const userNameTeste = JSON.parse(userName);
 }
