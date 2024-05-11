3f18ac0c4429b93f40b849dd7bfb3b1581ff5e38
Nicolas Gabriel <contato.nicolasneres@gmail.com>
2024-05-11 12:30:33 -0300

    fiz tanta coisa que nem tem como usar #

--- codigo-fonte/pagina_cadastro/script.js
+++ codigo-fonte/pagina_cadastro/script.js
@@ -1,5 +1,18 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("formulario").addEventListener('submit',function(event){
+        event.preventDefault();
+        registro();
+        this.submit();
+
+    });
+
+
+});
+
 function registro()
 {
+
     var nomeCompleto = document.getElementById("nome_completo").value;
     var email = document.getElementById("email").value;
     var telefone = document.getElementById("telefone").value;
@@ -9,12 +22,29 @@ function registro()
 
     if (!localStorage.getItem(email))
         {
-
+        
         let ListaRegistro={nome : nomeCompleto, email : email, telefone : telefone, cep : cep, Tipo_conta : tipoConta, senha : senha};
         localStorage.setItem(email,JSON.stringify(ListaRegistro));
+        window.location.href = "../pagina_login/index.html";
         }
     else
         {
         alert("Email ja cadastrado");
         }
 };
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("link-sobre").addEventListener("click",function(event){
+        event.preventDefault();
+        var sobre= document.getElementById("sobre");
+        if(sobre)
+        {   
+            document.getElementById("sobre").scrollIntoView({behavior:"smooth"});
+        }
+        else
+        {
+            window.location.href = "../pagina_inicial/index.html#sobre";
+        }
+    });
+});
