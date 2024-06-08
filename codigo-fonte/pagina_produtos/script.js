8f3db51c85d2abf6a102031d0e62eb16b5573cee
DaiPaula222 <139081871+DaiPaula222@users.noreply.github.com>
2024-06-08 20:00:49 -0300

    Update script.js

--- codigo-fonte/pagina_produtos/script.js
+++ codigo-fonte/pagina_produtos/script.js
@@ -56,4 +56,38 @@ function verificaLogin() {
   }
 }
 
-exibeProdutos();
+function buscarProdutos() {
+  var titulo = document.getElementById('').value;
+  
+
+  if (titulo === "" || categoria === "null" || modalidade === "null" || tempo === "" || resumo === "" || imagem === "") {
+    alert("Por favor, preencha todos os campos.");
+  } else {
+    let ListaRegistro = {
+      titulo : titulo,
+      categoria : categoria,
+      modalidade : modalidade,
+      tempo : tempo,
+      resumo :resumo,
+      imagem : imagem
+    };
+
+    criarProduto(ListaRegistro);
+
+    // localStorage.setItem('Produtos', JSON.stringify(ListaRegistro));
+    mostrarMensagem();
+  }
+}
+
+function mostrarMensagem() {
+  alert("Produto cadastrado com sucesso!");
+
+  document.getElementById('cad-titulo').value = "";
+  document.getElementById('cad-categoria').value = "null";
+  document.getElementById('cad-modalidade').value = "null";
+  document.getElementById('cad-tempo').value = "";
+  document.getElementById('cad-resumo').value = "";
+  document.getElementById('cad-imagem').value = "";
+}
+
+buscarProdutos();
