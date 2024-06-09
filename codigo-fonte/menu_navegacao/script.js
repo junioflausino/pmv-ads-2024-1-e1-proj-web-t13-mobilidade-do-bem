db60711340546d6e1a4671a1adf510eb6c59aa30
Letícia Moreira <987017@sga.pucminas.br>
2024-06-09 17:00:59 -0300

    Resolvendo problemas de conflito

--- codigo-fonte/menu_navegacao/script.js
+++ codigo-fonte/menu_navegacao/script.js
@@ -1,14 +1,3 @@
-var botaoPesquisa = document.getElementById('botao_pesquisa');
-
-// Ao clicar no botão pesquisa, o valor digitado pelo usuário é armazenado por parâmetro da URL na variável barraPesquisa e é feito o redirecionamento para página de produtos
-botaoPesquisa.onclick = function() {
-    console.log("clicado");
-    var barraPesquisa = document.getElementById('barra_pesquisa').value;
-
-    if (barraPesquisa !== null)
-        window.location.href = `../pagina_produtos/index.html?pesquisa=${barraPesquisa}`;
-}
-
 document.addEventListener('DOMContentLoaded', function(){   
     document.getElementById("link-sobre").addEventListener("click",function(event){
         event.preventDefault();
@@ -79,10 +68,13 @@ function botaoPerfil()
     }
 }
 
-var barraPesquisa = document.getElementById('barra_pesquisa').value;
 var botaoPesquisa = document.getElementById('botao_pesquisa');
 
+// Ao clicar no botão pesquisa, o valor digitado pelo usuário é armazenado por parâmetro da URL na variável barraPesquisa e é feito o redirecionamento para página de produtos
 botaoPesquisa.onclick = function() {
-    // buscarUmProduto(barraPesquisa);
-    window.location.href = "../pagina_perfil/perfil.html";
+    console.log("clicado");
+    var barraPesquisa = document.getElementById('barra_pesquisa').value;
+
+    if (barraPesquisa !== null)
+        window.location.href = `../pagina_produtos/index.html?pesquisa=${barraPesquisa}`;
 }
